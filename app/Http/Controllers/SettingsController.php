0e2f2e739dd8d47fe6362dfaa8a7909fabc19735
soybean15 <marlonpadilla1593@gmail.com>
2023-09-22 21:06:05 +0800

    added datetime

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Setting;
 use App\Traits\HasSettings;
 use Illuminate\Http\Request;
-
+use Carbon\Carbon;
 class SettingsController extends Controller
 {
     use HasSettings;
@@ -38,5 +38,31 @@ class SettingsController extends Controller
 
     public function updateSettings(Request $request){
         return $this->updateSetting($request['key'], $request['value']);
+    }
+
+
+    public function getCurrentDateTime(){
+
+
+  
+        $currentTime = Carbon::now();
+        $formattedDate = $currentTime->format('M, j Y, D');
+     
+        if ( $this->getSetting('24hrs_format')) {
+            // Format the time as "9:00AM" (12-hour format)
+            $formattedTime = $currentTime->format('H:i');
+            
+        } else {
+            // Format the time as "09:00" (24-hour format)
+            $formattedTime = $currentTime->format('h:iA');
+        }
+    
+
+        return response()->json([
+            'time'=>  $formattedTime,
+            'date'=>$formattedDate
+        ]);
+
+
     }
 }
