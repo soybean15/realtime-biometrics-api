40d8cc045efac5f75314c16207c6a76a94054c1f
soybean15 <marlonpadilla1593@gmail.com>
2023-09-26 23:41:35 +0800

    unknown time in

--- app/Actions/Employee/AttendanceType.php
+++ app/Actions/Employee/AttendanceType.php
@@ -11,25 +11,14 @@ trait AttendanceType
     use HasSettings;
     public function getType($employee, $time)
     {
-        // Get the attendance records for today for the given employee
+
         $attendance = $employee->attendanceToday()->get();
 
-        // $endTime = $this->carbonParse($this->getSetting('end_time'));
 
-        // return 'time '. $time . ' endTime ' . $endTime;
-        //  return  $attendance->count();
-
-        // Check if there are no attendance records for today
         if ($attendance->isEmpty()) {
-            return "Time in"; // No attendance records, return "Time in"
+            return "Time in"; 
         }
 
-
-        // Assuming you have an 'end_time' property in your settings
-        // Parse the provided time
-
-
-        // Define the break time range
         $breakStartTime = $this->carbonParse('12:00 PM');
         $breakEndTime = $this->carbonParse('1:00 PM');
         $attendanceDuringBreak = $attendance->filter(function ($record) use ($breakStartTime, $breakEndTime) {
@@ -43,9 +32,9 @@ trait AttendanceType
             }
             return 'Break Out';
 
-        } elseif ($time > $this->carbonParse($this->getSetting('end_time'))) {
+        } elseif ($time >= $this->carbonParse($this->getSetting('end_time'))) {
             return "Time out"; // Punch-in time is after the end time, return "Time out"
-        }else  return "Break in";
+        }else  return "Unknown";
 
 
     }
