46432c9568300884e841928c0af75cc3a0cf9d60
Jean Esteban <jean@pop-os.localdomain>
2023-10-22 23:05:31 +0800

    add department added

--- app/Http/Controllers/Admin/DepartmentController.php
+++ app/Http/Controllers/Admin/DepartmentController.php
@@ -5,10 +5,17 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Department;
 use Illuminate\Http\Request;
+use App\Http\Managers\DepartmentManager;
 
 class DepartmentController extends Controller
 {
     //
+
+    protected DepartmentManager $manager;
+
+    public function __construct(DepartmentManager $manager){
+        $this->manager=$manager;
+    }
 
     public function index(){
         
@@ -16,4 +23,32 @@ class DepartmentController extends Controller
             'departments'=> Department::all()
         ]);
     }
+
+
+    public function store(Request $request){
+        $this->manager->store($request->all());
+    }
+
+    public function destroy(Request $request){
+        return $this->manager->destroy($request['id']);
+    }
+ 
+    public function update(Request $request){
+        return $this->manager->update(
+            [
+                'attribute'=>$request['attribute'],
+                'value'=>$request['value']
+            ], 
+            $request['id']
+        );
+    }
+
+
+    public function search (Request $request){
+
+        return $this->manager->search($request['val']==null?'':$request['val']);
+
+      
+    }
+
 }
