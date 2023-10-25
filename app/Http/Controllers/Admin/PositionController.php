30a1e8e2c3e50f6894f0fe113b644eea2b5a660c
Jean Esteban <jean@pop-os.localdomain>
2023-10-25 20:39:54 +0800

    position add, search added

--- app/Http/Controllers/Admin/PositionController.php
+++ app/Http/Controllers/Admin/PositionController.php
@@ -1,18 +1,50 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Admin; 
 
 use App\Http\Controllers\Controller;
 use App\Models\Position;
 use Illuminate\Http\Request;
+use App\Http\Managers\PositionManager;
 
 class PositionController extends Controller
 {
     //
+
+    protected PositionManager $manager;
+
+    public function __construct(PositionManager $manager){
+        $this->manager=$manager;
+    }
+
+
     public function index(){
 
         return response()->json([
-            'positions'=> Position::all()
+            'positions'=> Position::paginate(5)
         ]);
     }
+
+    public function store(Request $request){
+        $this->manager->store($request->all());
+    }
+
+
+    public function search (Request $request){
+
+        return $this->manager->search($request['val']==null?'':$request['val']);   
+    }
+
+
+    public function update(Request $request){
+        return $this->manager->update($request->all(), 
+            $request['id']
+        );
+    }
+
+    public function destroy(Request $request){
+        return $this->manager->destroy($request['id']);
+    }
+
+
 }
--- app/Http/Managers/PositionManager.php
+++ app/Http/Managers/PositionManager.php
@@ -1,7 +1,116 @@
 <?php
 
 namespace App\Http\Managers;
+use App\Models\Position;
+use Illuminate\Support\Facades\Validator;
 
 class PositionManager{
+
+
+    public function store($data){
+      
+        Validator::make($data, [
+            'name' =>['required', 'string', 'max:255'],
+        ])->validate();
+    
+   
+        $position = Position::create([
+            'name' => $data['name'],
+        ]);
+    
+        return response()->json([
+            'position' => $position
+        ]);
+    }
+
+
+    public function search (String $val =''){
+
+        $positions = Position::where('name', 'LIKE', "$val%")->get();
+
+      
+        return response()->json([
+            'positions'=>$positions
+        ]); 
+
+
+    }
+
+    function update($data, $id)
+    {
+        $position = Position::find($id);
+
+
+        Validator::make($data, [
+            'name' =>['required', 'string', 'max:255'],
+        ])->validate();
+    
+    
+        try {
+     
+            if ($position) {
+
+
+
+                $position['name'] = $data['name'];
+
+                $position->save();
+
+
+                return response()->json([
+                    'message' => 'Position Updated Succesfully',
+                    'status' => 'success'
+
+                ]);
+
+            } else {
+
+                throw new \Exception("No Position Found", 422);
+            }
+
+        } catch (\Exception $e) {
+            if ($e->getCode() == 422) {
+
+                $error = json_decode($e->getMessage(), true);
+                $errorMessage = $error['value'][0];
+
+
+                return response()->json([
+                    'message' => $errorMessage,
+                    'status' => 'failed'
+
+                ], $e->getCode());
+            }
+
+
+            return response()->json([
+                'message' => $e->getMessage(),
+                'status' => 'failed'
+            ], 404);
+
+        }
+
+       }
+
+       public function destroy($id){
+
+
+        $position = Position::find($id);
+
+
+            $position->delete();
+
+
+            return response()->json([
+
+                'message'=>'Position Successfully Deleted',
+                'status'=>'success'
+            ]);
+
+    }
+
+
+
+
     
 }
